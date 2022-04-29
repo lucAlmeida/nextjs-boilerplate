91c7a6e565c14d31edcae384364a9213ad76245d
lucAlmeida <lucas.a.souza@outlook.com>
2022-04-29 00:06:01 -0300

    Next.js Boilerplate

---
+++ .storybook/preview.js
@@ -0,0 +1,27 @@
+import GlobalStyles from "../src/styles/global";
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  )
+]
+
+/*
+import { addDecorator } from '@storybook/react'
+import withGlobalStyles from './withGlobalStyles'
+
+addDecorator(withGlobalStyles)
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+*/
